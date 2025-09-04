64001ecd57457d0066cbff325b6b79b58d74272e
Rui Li <rli3@caltech.edu>
2025-09-04 12:45:22 -0700

    remove quickfix mode

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -80,6 +80,7 @@ return {
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = 'zathura'
       vim.g.vimtex_compiler_method = 'latexmk'
+      vim.g.vimtex_quickfix_mode = 0
     end,
   },
 
